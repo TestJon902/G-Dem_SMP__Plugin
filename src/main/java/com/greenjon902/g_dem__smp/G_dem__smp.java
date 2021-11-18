5598b8965a520b7feac9c3609f66929950aeb78b
GreenJon902 <GreenJon902@gmail.com>
2021-11-18 16:46:21 +0000

    Initial commit

---
+++ src/main/java/com/greenjon902/g_dem__smp/G_dem__smp.java
@@ -0,0 +1,17 @@
+package com.greenjon902.g_dem__smp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class G_dem__smp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/mc/greenjon902/g_dem__smp/G_Dem__SMP.java
@@ -0,0 +1,21 @@
+package mc.greenjon902.g_dem__smp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class G_Dem__SMP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Starting the G-Dem SMP plugin...");
+        // Plugin startup logic
+        System.out.println("Started the G-Dem SMP plugin...");
+
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Ending the G-Dem SMP plugin...");
+        // Plugin shutdown logic
+        System.out.println("Ended the G-Dem SMP plugin...");
+    }
+}
